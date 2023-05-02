25b6f2116a5aa289484b10977357442ddbd914c0
EsraaAbdelfattah <115654409+EsraaAbdelfattah@users.noreply.github.com>
2023-05-02 22:54:21 +0300

    Add files via upload

---
+++ my archive1/main.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace my_archive1
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tileBar1 = new DevExpress.XtraBars.Navigation.TileBar();
+            this.pn_contaier = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // tileBar1
+            // 
+            this.tileBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tileBar1.DropDownOptions.BeakColor = System.Drawing.Color.Empty;
+            this.tileBar1.Location = new System.Drawing.Point(0, 0);
+            this.tileBar1.Name = "tileBar1";
+            this.tileBar1.ScrollMode = DevExpress.XtraEditors.TileControlScrollMode.ScrollButtons;
+            this.tileBar1.Size = new System.Drawing.Size(1078, 98);
+            this.tileBar1.TabIndex = 0;
+            this.tileBar1.Text = "tileBar1";
+            // 
+            // pn_contaier
+            // 
+            this.pn_contaier.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pn_contaier.Location = new System.Drawing.Point(0, 98);
+            this.pn_contaier.Name = "pn_contaier";
+            this.pn_contaier.Size = new System.Drawing.Size(1078, 582);
+            this.pn_contaier.TabIndex = 1;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 680);
+            this.Controls.Add(this.pn_contaier);
+            this.Controls.Add(this.tileBar1);
+            this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Shadow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "main";
+            this.Text = "Arshif";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Navigation.TileBar tileBar1;
+        private System.Windows.Forms.Panel pn_contaier;
+    }
+}
+
