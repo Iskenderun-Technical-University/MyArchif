c8e136b306e634455f4668644f97121c1a8d0465
EsraaAbdelfattah <eabdelfattah.mdbf20@iste.edu.tr>
2023-05-27 02:25:38 +0300

    adding the tile bar

--- my archive1/main.Designer.cs
+++ my archive1/main.Designer.cs
@@ -28,40 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DevExpress.XtraEditors.TileItemElement tileItemElement7 = new DevExpress.XtraEditors.TileItemElement();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            DevExpress.XtraEditors.TileItemElement tileItemElement8 = new DevExpress.XtraEditors.TileItemElement();
+            DevExpress.XtraEditors.TileItemElement tileItemElement9 = new DevExpress.XtraEditors.TileItemElement();
+            DevExpress.XtraEditors.TileItemElement tileItemElement10 = new DevExpress.XtraEditors.TileItemElement();
+            DevExpress.XtraEditors.TileItemElement tileItemElement11 = new DevExpress.XtraEditors.TileItemElement();
+            DevExpress.XtraEditors.TileItemElement tileItemElement12 = new DevExpress.XtraEditors.TileItemElement();
             this.tileBar1 = new DevExpress.XtraBars.Navigation.TileBar();
             this.pn_contaier = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pn_contaier.SuspendLayout();
+            this.tileBarGroup2 = new DevExpress.XtraBars.Navigation.TileBarGroup();
+            this.tileBarItem1 = new DevExpress.XtraBars.Navigation.TileBarItem();
+            this.tileBarItem2 = new DevExpress.XtraBars.Navigation.TileBarItem();
+            this.US = new DevExpress.XtraBars.Navigation.TileBarItem();
+            this.tileBarItem4 = new DevExpress.XtraBars.Navigation.TileBarItem();
+            this.tileBarItem5 = new DevExpress.XtraBars.Navigation.TileBarItem();
+            this.tileBarItem6 = new DevExpress.XtraBars.Navigation.TileBarItem();
             this.SuspendLayout();
             // 
             // tileBar1
             // 
+            this.tileBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(45)))), ((int)(((byte)(78)))));
             this.tileBar1.Dock = System.Windows.Forms.DockStyle.Top;
             this.tileBar1.DropDownOptions.BeakColor = System.Drawing.Color.Empty;
+            this.tileBar1.Groups.Add(this.tileBarGroup2);
             this.tileBar1.Location = new System.Drawing.Point(0, 0);
+            this.tileBar1.MaxId = 6;
             this.tileBar1.Name = "tileBar1";
             this.tileBar1.ScrollMode = DevExpress.XtraEditors.TileControlScrollMode.ScrollButtons;
-            this.tileBar1.Size = new System.Drawing.Size(1078, 98);
+            this.tileBar1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(226)))), ((int)(((byte)(239)))));
+            this.tileBar1.Size = new System.Drawing.Size(1078, 103);
             this.tileBar1.TabIndex = 0;
             this.tileBar1.Text = "tileBar1";
             // 
             // pn_contaier
             // 
-            this.pn_contaier.Controls.Add(this.label1);
+            this.pn_contaier.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
             this.pn_contaier.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pn_contaier.Location = new System.Drawing.Point(0, 98);
+            this.pn_contaier.Location = new System.Drawing.Point(0, 103);
             this.pn_contaier.Name = "pn_contaier";
-            this.pn_contaier.Size = new System.Drawing.Size(1078, 582);
+            this.pn_contaier.Size = new System.Drawing.Size(1078, 577);
             this.pn_contaier.TabIndex = 1;
             // 
-            // label1
+            // tileBarGroup2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(92, 37);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Test Label";
+            this.tileBarGroup2.Items.Add(this.tileBarItem1);
+            this.tileBarGroup2.Items.Add(this.tileBarItem2);
+            this.tileBarGroup2.Items.Add(this.US);
+            this.tileBarGroup2.Items.Add(this.tileBarItem4);
+            this.tileBarGroup2.Items.Add(this.tileBarItem5);
+            this.tileBarGroup2.Items.Add(this.tileBarItem6);
+            this.tileBarGroup2.Name = "tileBarGroup2";
+            // 
+            // tileBarItem1
+            // 
+            this.tileBarItem1.AppearanceItem.Disabled.Font = new System.Drawing.Font("hawi_t_Diwany", 8.25F);
+            this.tileBarItem1.AppearanceItem.Disabled.Options.UseFont = true;
+            this.tileBarItem1.AppearanceItem.Normal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(114)))), ((int)(((byte)(175)))));
+            this.tileBarItem1.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tileBarItem1.AppearanceItem.Normal.Options.UseBackColor = true;
+            this.tileBarItem1.AppearanceItem.Normal.Options.UseFont = true;
+            this.tileBarItem1.DropDownOptions.BeakColor = System.Drawing.Color.Empty;
+            tileItemElement7.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image")));
+            tileItemElement7.Text = "HOME";
+            tileItemElement7.TextAlignment = DevExpress.XtraEditors.TileItemContentAlignment.MiddleCenter;
+            this.tileBarItem1.Elements.Add(tileItemElement7);
+            this.tileBarItem1.Id = 0;
+            this.tileBarItem1.ItemSize = DevExpress.XtraBars.Navigation.TileBarItemSize.Wide;
+            this.tileBarItem1.Name = "tileBarItem1";
+            this.tileBarItem1.Padding = new System.Windows.Forms.Padding(11);
+            // 
+            // tileBarItem2
+            // 
+            this.tileBarItem2.AppearanceItem.Normal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(114)))), ((int)(((byte)(175)))));
+            this.tileBarItem2.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tileBarItem2.AppearanceItem.Normal.Options.UseBackColor = true;
+            this.tileBarItem2.AppearanceItem.Normal.Options.UseFont = true;
+            this.tileBarItem2.DropDownOptions.BeakColor = System.Drawing.Color.Empty;
+            tileItemElement8.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image1")));
+            tileItemElement8.Text = "SECTIONS";
+            tileItemElement8.TextAlignment = DevExpress.XtraEditors.TileItemContentAlignment.MiddleRight;
+            this.tileBarItem2.Elements.Add(tileItemElement8);
+            this.tileBarItem2.Id = 1;
+            this.tileBarItem2.ItemSize = DevExpress.XtraBars.Navigation.TileBarItemSize.Wide;
+            this.tileBarItem2.Name = "tileBarItem2";
+            this.tileBarItem2.Padding = new System.Windows.Forms.Padding(11);
+            // 
+            // US
+            // 
+            this.US.AppearanceItem.Normal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(114)))), ((int)(((byte)(175)))));
+            this.US.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.US.AppearanceItem.Normal.Options.UseBackColor = true;
+            this.US.AppearanceItem.Normal.Options.UseFont = true;
+            this.US.DropDownOptions.BeakColor = System.Drawing.Color.Empty;
+            tileItemElement9.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image2")));
+            tileItemElement9.Text = "USERS";
+            tileItemElement9.TextAlignment = DevExpress.XtraEditors.TileItemContentAlignment.MiddleCenter;
+            this.US.Elements.Add(tileItemElement9);
+            this.US.Id = 2;
+            this.US.ItemSize = DevExpress.XtraBars.Navigation.TileBarItemSize.Wide;
+            this.US.Name = "US";
+            this.US.Padding = new System.Windows.Forms.Padding(11);
+            // 
+            // tileBarItem4
+            // 
+            this.tileBarItem4.AppearanceItem.Normal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(114)))), ((int)(((byte)(175)))));
+            this.tileBarItem4.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tileBarItem4.AppearanceItem.Normal.Options.UseBackColor = true;
+            this.tileBarItem4.AppearanceItem.Normal.Options.UseFont = true;
+            this.tileBarItem4.DropDownOptions.BeakColor = System.Drawing.Color.Empty;
+            tileItemElement10.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image3")));
+            tileItemElement10.Text = "ARCHIVES";
+            tileItemElement10.TextAlignment = DevExpress.XtraEditors.TileItemContentAlignment.MiddleRight;
+            this.tileBarItem4.Elements.Add(tileItemElement10);
+            this.tileBarItem4.Id = 3;
+            this.tileBarItem4.ItemSize = DevExpress.XtraBars.Navigation.TileBarItemSize.Wide;
+            this.tileBarItem4.Name = "tileBarItem4";
+            this.tileBarItem4.Padding = new System.Windows.Forms.Padding(11);
+            // 
+            // tileBarItem5
+            // 
+            this.tileBarItem5.AppearanceItem.Normal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(114)))), ((int)(((byte)(175)))));
+            this.tileBarItem5.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tileBarItem5.AppearanceItem.Normal.Options.UseBackColor = true;
+            this.tileBarItem5.AppearanceItem.Normal.Options.UseFont = true;
+            this.tileBarItem5.DropDownOptions.BeakColor = System.Drawing.Color.Empty;
+            tileItemElement11.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image4")));
+            tileItemElement11.Text = "SETTINGS";
+            tileItemElement11.TextAlignment = DevExpress.XtraEditors.TileItemContentAlignment.MiddleRight;
+            this.tileBarItem5.Elements.Add(tileItemElement11);
+            this.tileBarItem5.Id = 4;
+            this.tileBarItem5.ItemSize = DevExpress.XtraBars.Navigation.TileBarItemSize.Wide;
+            this.tileBarItem5.Name = "tileBarItem5";
+            this.tileBarItem5.Padding = new System.Windows.Forms.Padding(11);
+            // 
+            // tileBarItem6
+            // 
+            this.tileBarItem6.AppearanceItem.Disabled.BackColor = System.Drawing.Color.White;
+            this.tileBarItem6.AppearanceItem.Disabled.Options.UseBackColor = true;
+            this.tileBarItem6.AppearanceItem.Normal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(114)))), ((int)(((byte)(175)))));
+            this.tileBarItem6.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tileBarItem6.AppearanceItem.Normal.Options.UseBackColor = true;
+            this.tileBarItem6.AppearanceItem.Normal.Options.UseFont = true;
+            this.tileBarItem6.DropDownOptions.BeakColor = System.Drawing.Color.Empty;
+            tileItemElement12.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image5")));
+            tileItemElement12.Text = "HELP";
+            tileItemElement12.TextAlignment = DevExpress.XtraEditors.TileItemContentAlignment.MiddleCenter;
+            this.tileBarItem6.Elements.Add(tileItemElement12);
+            this.tileBarItem6.Id = 5;
+            this.tileBarItem6.ItemSize = DevExpress.XtraBars.Navigation.TileBarItemSize.Wide;
+            this.tileBarItem6.Name = "tileBarItem6";
+            this.tileBarItem6.Padding = new System.Windows.Forms.Padding(11);
             // 
             // main
             // 
@@ -73,10 +190,8 @@
             this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Shadow;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "main";
-            this.Text = "Arshif";
+            this.Text = "My Arshif";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.pn_contaier.ResumeLayout(false);
-            this.pn_contaier.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -85,7 +200,13 @@
 
         private DevExpress.XtraBars.Navigation.TileBar tileBar1;
         private System.Windows.Forms.Panel pn_contaier;
-        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraBars.Navigation.TileBarGroup tileBarGroup2;
+        private DevExpress.XtraBars.Navigation.TileBarItem tileBarItem1;
+        private DevExpress.XtraBars.Navigation.TileBarItem tileBarItem2;
+        private DevExpress.XtraBars.Navigation.TileBarItem US;
+        private DevExpress.XtraBars.Navigation.TileBarItem tileBarItem4;
+        private DevExpress.XtraBars.Navigation.TileBarItem tileBarItem5;
+        private DevExpress.XtraBars.Navigation.TileBarItem tileBarItem6;
     }
 }
 
